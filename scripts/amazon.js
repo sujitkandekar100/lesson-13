0ae2eab1d437714939c4899e192c07b0ecd1793b
sujitkandekar100 <sujitkandekar100@gmail.com>
2025-01-02 11:16:49 +0530

    new features is when select product quantity get proper quantity of product

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -60,7 +60,6 @@ document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
-
       let matchingItem;
 
       cart.forEach((item) => {
@@ -77,6 +76,9 @@ document.querySelectorAll('.js-add-to-cart')
           quantity: 1
         });
       }
+      let quantity=document.querySelector(`.js-quantity-selector-${productId}`);
+      let productQuantity=quantity.value;
+      console.log(productQuantity);
 
       let cartQuantity = 0;
 
